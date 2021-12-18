677ebd44c99c22877e86a6c247a85cabc3da800f
Léo Nicaisse <hello@leonicaisse.fr>
2021-12-18 12:05:03 +0100

    fix: rebase

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
+<<<<<<< HEAD
 const morgan = require('morgan');
+=======
+const morgan = require("morgan");
+>>>>>>> d05f012 (fix: server/app.js)
 const cors = require('cors');
 
 app.use(morgan());
@@ -12,5 +16,9 @@ const database = require('./config/database');
 
 const routes = require('./routes');
 app.use('/', routes);
+<<<<<<< HEAD
+=======
+
+>>>>>>> d05f012 (fix: server/app.js)
 
 module.exports = app;
