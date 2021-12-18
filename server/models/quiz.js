d9d14ce56f0c73a449b3bd5491d60bffbcfa33e0
Léo Nicaisse <hello@leonicaisse.fr>
2021-12-18 13:33:19 +0100

    fix: cleaning files after conflicts resolution

--- server/models/quiz.js
+++ server/models/quiz.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const quizSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    questions: [{
-        name: String,
-        correct_answer: String,
-        answers: [{
-            name: String
-        }]
+  name: {
+    type: String,
+    required: true,
+  },
+  questions: [{
+    name: String,
+    correct_answer: String,
+    answers: [{
+      name: String,
     }],
+  }],
 });
 
 module.exports = mongoose.model('Quiz', quizSchema);
--- server/routes/quiz/quiz.js
+++ server/routes/quiz/quiz.js
@@ -2,44 +2,44 @@ const routes = require('express').Router();
 const Quiz = require('../../models/quiz');
 
 routes.get('/', async (req, res) => {
-    try {
-        const quiz = await Quiz.find();
-        res.json(quiz);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+  try {
+    const quiz = await Quiz.find();
+    res.json(quiz);
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
 });
 
 routes.post('/', async (req, res) => {
-    const quiz = new Quiz({
-        "name": "Quiz",
-        "questions": [
-            {
-                "name": "Question: ",
-                "correct_answer": "C",
-                "answers": [
-                    {
-                        "name": "A"
-                    },
-                    {
-                        "name": "B"
-                    },
-                    {
-                        "name": "C"
-                    },
-                    {
-                        "name": "D"
-                    }
-                ]
-            },
-        ]
-    });
-    try {
-        const newQuiz = await quiz.save();
-        res.status(200).json(newQuiz);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+  const quiz = new Quiz({
+    'name': 'Quiz',
+    'questions': [
+      {
+        'name': 'Question: ',
+        'correct_answer': 'C',
+        'answers': [
+          {
+            'name': 'A',
+          },
+          {
+            'name': 'B',
+          },
+          {
+            'name': 'C',
+          },
+          {
+            'name': 'D',
+          },
+        ],
+      },
+    ],
+  });
+  try {
+    const newQuiz = await quiz.save();
+    res.status(200).json(newQuiz);
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
 });
 
 
