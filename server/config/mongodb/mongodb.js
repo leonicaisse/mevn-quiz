f28126c9ced4770b1f7eb88d9de6da32c7947568
Léo Nicaisse <hello@leonicaisse.fr>
2021-12-24 17:54:34 +0100

    feat: better usage of .env in database connection

--- server/config/mongodb/mongodb.js
+++ server/config/mongodb/mongodb.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 require('dotenv-flow').config();
 
+const {
+  DATABASE_URL,
+  DATABASE_NAME,
+  DATABASE_USERNAME,
+  DATABASE_PASSWORD,
+} = process.env;
+
+const MONGODB_URL =`mongodb+srv://${DATABASE_USERNAME}:${encodeURIComponent(DATABASE_PASSWORD)}@${DATABASE_URL}/${DATABASE_NAME}?retryWrites=true&w=majority`;
+
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL, {
+    await mongoose.connect(MONGODB_URL, {
       useNewUrlParser: true,
     });
     console.log('MongoDB connected!!');
