7ed02afc0bfa9832c90728b032fe6f86edfb8000
Léo Nicaisse <hello@leonicaisse.fr>
2021-12-24 17:56:32 +0100

    feat(back): init API swagger documentation - QUIZ-21

---
+++ server/routes/api-docs/index.js
@@ -0,0 +1,20 @@
+const routes = new (require('express')).Router();
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'MEVN-Quiz API',
+      description: 'MEVN-Quiz API Documentation',
+      servers: [`http://localhost:${process.env.port || 8000}`],
+    },
+  },
+  apis: ['./routes/**/*.js'],
+};
+
+routes
+    .use('/', swaggerUI.serve)
+    .get('/', swaggerUI.setup(swaggerJsDoc(swaggerOptions)));
+
+module.exports = routes;
--- server/routes/quiz/quiz.js
+++ server/routes/quiz/quiz.js
@@ -1,6 +1,19 @@
 const routes = new (require('express')).Router();
 const quiz = require('../../api/quiz');
 
+/**
+ * @swagger
+ * /quiz:
+ *  get:
+ *    summary: Get all quizzes list
+ *    tags:
+ *      - quizzes
+ *    responses:
+ *      200:
+ *        description: All quizzes
+ *      500:
+ *        description: An error has occured
+ */
 routes.get('/', async (req, res) => {
   try {
     res.status(200).send(await quiz.index());
@@ -9,15 +22,25 @@ routes.get('/', async (req, res) => {
   }
 });
 
-routes.get('/:id', async (req, res) => {
-  const {id} = req.params;
-  try {
-    res.status(200).send(await quiz.read(id));
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
+/**
+ * @swagger
+ * /quiz:
+ *  post:
+ *    summary: Create a new quiz
+ *    parameters:
+ *      - in: body
+ *        name: Request Body
+ *        description: "The data for the new quiz"
+ *        type: object
+ *        required: true
+ *    tags:
+ *      - quizzes
+ *    responses:
+ *      201:
+ *        description: Quiz created
+ *      500:
+ *        description: An error has occured
+ */
 routes.post('/', async (req, res) => {
   const {data} = req.body;
   try {
@@ -27,6 +50,60 @@ routes.post('/', async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /quiz/{id}:
+ *  get:
+ *    summary: Get a quiz by ID
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: ID of the quiz to get
+ *    tags:
+ *      - quizzes
+ *    responses:
+ *      200:
+ *        description: Requested quiz
+ *      500:
+ *        description: An error has occured
+ */
+routes.get('/:id', async (req, res) => {
+  const {id} = req.params;
+  try {
+    res.status(200).send(await quiz.read(id));
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+/**
+ * @swagger
+ * /quiz/{id}:
+ *  patch:
+ *    summary: Update a quiz by ID
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: ID of the quiz to update
+ *      - in: body
+ *        name: Request Body
+ *        description: "The new data for the quiz"
+ *        type: object
+ *        required: true
+ *    tags:
+ *      - quizzes
+ *    responses:
+ *      200:
+ *        description: Quiz updated
+ *      500:
+ *        description: An error has occured
+ */
 routes.patch('/:id', async (req, res) => {
   // TODO: Handle deep updates (i.e. question update)
   const {id} = req.params;
@@ -38,6 +115,26 @@ routes.patch('/:id', async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /quiz/{id}:
+ *  delete:
+ *    summary: Delete a quiz by ID
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: ID of the quiz to delete
+ *    tags:
+ *      - quizzes
+ *    responses:
+ *      204:
+ *        description: Quiz deleted
+ *      500:
+ *        description: An error has occured
+ */
 routes.delete('/:id', async (req, res) => {
   const {id} = req.params;
   try {
